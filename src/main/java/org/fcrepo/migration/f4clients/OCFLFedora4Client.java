f3545303cc9c244aac6a13f094cbd35deae1c7ef
Remi Malessa <rem@llgc.org.uk>
2019-09-19 15:02:37 +0100

    Implemented contructor for org/fcrepo/migration/f4clients/OCFLFedora4Client.java

--- src/main/java/org/fcrepo/migration/f4clients/OCFLFedora4Client.java
+++ src/main/java/org/fcrepo/migration/f4clients/OCFLFedora4Client.java
@@ -1,11 +1,22 @@
+
 package org.fcrepo.migration.f4clients;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.fcrepo.migration.Fedora4Client;
 import org.slf4j.Logger;
 
-import java.io.InputStream;
-
-import static org.slf4j.LoggerFactory.getLogger;
+import edu.wisc.library.ocfl.api.OcflRepository;
+import edu.wisc.library.ocfl.core.OcflRepositoryBuilder;
+import edu.wisc.library.ocfl.core.mapping.ObjectIdPathMapperBuilder;
+import edu.wisc.library.ocfl.core.storage.FileSystemOcflStorage;
 
 /**
  * @author RichardDWilliams
@@ -15,15 +26,37 @@ public class OCFLFedora4Client implements Fedora4Client {
 
     private static final Logger LOGGER = getLogger(OCFLFedora4Client.class);
 
+    private final OcflRepository ocflRepo;
+
+    private final String storageRoot;
+
+    private final String stagingRoot;
 
     /**
      * Constructor
      * 
+     * @author Remigiusz Malessa
+     * @since 4.4.1-SNAPSHOT
      * @param storage Root for OCFL Objects
      * @param staging directory for in-progress OCFL Objects
      */
     public OCFLFedora4Client(final String storage, final String staging) {
-        LOGGER.info("to-be-implemented: OCFLFedora4Client: " + storage + "," + staging);
+
+        this.storageRoot = storage;
+        this.stagingRoot = staging;
+        // If locations exist instantiate repository
+        if (new File(storage).isDirectory() && new File(staging).isDirectory()) {
+            final Path repoDir = Paths.get(this.storageRoot);
+            Security.addProvider(new BouncyCastleProvider());
+            ocflRepo =
+                    new OcflRepositoryBuilder().build(new FileSystemOcflStorage(repoDir,
+                            new ObjectIdPathMapperBuilder().withDefaultCaffeineCache().buildDefaultPairTreeMapper()),
+                            repoDir.resolve("deposit"));
+        } else {
+            final String missingDir = new File(storage).isDirectory() ? staging : storage;
+            throw new RuntimeException("Failed to create OCFL repository, because location: " + missingDir +
+                    " does not exists, or is not a directory.");
+        }
     }
 
     /**
@@ -46,6 +79,7 @@ public class OCFLFedora4Client implements Fedora4Client {
 
     /**
      * Gets the repository URL (to which paths can be appended to reference resources).
+     *
      * @return the repository URL
      */
     @Override
@@ -56,6 +90,7 @@ public class OCFLFedora4Client implements Fedora4Client {
 
     /**
      * Creates or updates a non-RDF resource that points to external content at the given URL.
+     *
      * @param path the path of the resource to be created
      * @param url the URL at which the external content is hosted
      */
@@ -66,6 +101,7 @@ public class OCFLFedora4Client implements Fedora4Client {
 
     /**
      * Creates or updates a non-RDF resource.
+     *
      * @param path the path of the resource to be modified/created
      * @param content the non-RDF content
      * @param contentType the mime type of the content
@@ -77,6 +113,7 @@ public class OCFLFedora4Client implements Fedora4Client {
 
     /**
      * Creates a version snapshot for the resource (or graph) at the given path.
+     *
      * @param path the path of the resource to be versioned
      * @param versionId a label for the version
      */
@@ -87,6 +124,7 @@ public class OCFLFedora4Client implements Fedora4Client {
 
     /**
      * Updates properties on a resource.
+     *
      * @param path the resource whose properties are to be updated.
      * @param sparqlUpdate the sparql update statements to be applied
      */
@@ -97,6 +135,7 @@ public class OCFLFedora4Client implements Fedora4Client {
 
     /**
      * Updates properties on a non-RDF resource.
+     *
      * @param path the resource whose properties are to be updated.
      * @param sparqlUpdate the sparql update statements to be applied
      */
@@ -106,14 +145,14 @@ public class OCFLFedora4Client implements Fedora4Client {
     }
 
     /**
-     * Creates a placeholder resource at the given path (or at a server-assigned path,
-     * if no path is given) if no resource exists at that path.  If a resource already
-     * exists, this method returns the path to that resource which may or may not be
-     * a placeholder.  If none exists, this method creates a new resource that should
-     * should be distinguishable from resources that have already been migrated as well
-     * as resources created using another process.
-     * @param path a path at which to create a placeholder resource (or null to create
-     *             a placeholder resource at a server-assigned path).
+     * Creates a placeholder resource at the given path (or at a server-assigned path, if no path is given) if no
+     * resource exists at that path. If a resource already exists, this method returns the path to that resource which
+     * may or may not be a placeholder. If none exists, this method creates a new resource that should should be
+     * distinguishable from resources that have already been migrated as well as resources created using another
+     * process.
+     *
+     * @param path a path at which to create a placeholder resource (or null to create a placeholder resource at a
+     *        server-assigned path).
      * @return the path of the placeholder resource that was created
      */
     @Override
@@ -123,14 +162,14 @@ public class OCFLFedora4Client implements Fedora4Client {
     }
 
     /**
-     * Creates a placeholder non-RDF resource at the given path (or at a server-assigned
-     * path, if no path is given) if no resource exists at that path.  If a resource
-     * already exists, this method returns the path to that resource which may or may not
-     * be a placeholder.  If none exists, this method creates a new resource that should
-     * should be distinguishable from resources that have already been migrated as well
-     * as resources created using another process.
-     * @param path a path at which to create a placeholder resource (or null to create
-     *             a placeholder resource at a server-assigned path).
+     * Creates a placeholder non-RDF resource at the given path (or at a server-assigned path, if no path is given) if
+     * no resource exists at that path. If a resource already exists, this method returns the path to that resource
+     * which may or may not be a placeholder. If none exists, this method creates a new resource that should should be
+     * distinguishable from resources that have already been migrated as well as resources created using another
+     * process.
+     *
+     * @param path a path at which to create a placeholder resource (or null to create a placeholder resource at a
+     *        server-assigned path).
      * @return the path of the placeholder resource that was created
      */
     @Override
@@ -141,6 +180,7 @@ public class OCFLFedora4Client implements Fedora4Client {
 
     /**
      * Determines whether the resource at the given path is a placeholder or not.
+     *
      * @param path a path of a resource (expected to exist)
      * @return true if it's a placeholder, false otherwise
      */
