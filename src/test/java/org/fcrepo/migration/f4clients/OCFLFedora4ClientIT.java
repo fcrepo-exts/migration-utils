b7c8235427e12d58450427f61289b5fdf8b40a1f
Remi M <29232446+rmassa@users.noreply.github.com>
2019-09-24 14:33:00 +0100

    Implementation of OCFLFedora4Client exists(String path) method (#61)
    * Implemented contructor for org/fcrepo/migration/f4clients/OCFLFedora4Client.java
    
    * Implemented exist method in src/main/java/org/fcrepo/migration/f4clients/OCFLFedora4Client.java
    Added Test class for src/main/java/org/fcrepo/migration/f4clients/OCFLFedora4Client.java , exist method.
    
    * Replaced the test object in the test/resources with a simpler one.
    Changed the ocfl hierarchy in the contructor of
    org/fcrepo/migration/f4clients/OCFLFedora4Client.java to use the .buildFlatMapper() method.
    This should be changed to allow option of different storage types.
    
    * Changes:
    * Added new parameter to the constructor of org.fcrepo.migration.f4clients.OCFLFedora4Client class , to be able to initialize OCFL repository with custom mapper. Only Flat Mapper available at the moment.
    * Added integration testing for exist() method of org.fcrepo.migration.f4clients.OCFLFedora4Client. Needs adding more methods as we progress with implementation.
    * Updated JUnit testing to compensate for new method addition
    * Removed BouncyCastleProvider security provider.
    * Added logging requested in the pull request review.
    * spring/ocfl-it-setup.xml is now configured to work with the OCFLFedora4Client. OCFLGoLangFedora4ClientIT moved to spring/ocfl-go-it-setup.xml
    
    * Added handling of unknown object id mapper being passed to the
    contructor of OCFLFedora4Client
    
    * Removed unnecessary comment

---
+++ src/test/java/org/fcrepo/migration/f4clients/OCFLFedora4ClientIT.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2015 DuraSpace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fcrepo.migration.f4clients;
+
+import static org.junit.Assert.assertFalse;
+
+import java.util.UUID;
+
+import org.fcrepo.migration.Fedora4Client;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * A basic suite of integration tests to test certain interaction patterns (and code) against the an OCFL version of
+ * Fedora.
+ *
+ * @author awoods, copied from org.fcrepo.migration.f4clients.OCFLGoLangFedora4ClientIT.java by Remi Malessa
+ * @since 4.4.1-SNAPSHOT
+ */
+public class OCFLFedora4ClientIT {
+
+    private Fedora4Client client;
+
+    @Before
+    public void setup() throws BeansException {
+
+        final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("spring/ocfl-it-setup.xml");
+        client = (Fedora4Client) context.getBean("fedora4Client");
+    }
+
+    /**
+     * Testing integration of the exist() method.
+     *
+     * @author awoods, copied from org.fcrepo.migration.f4clients.OCFLGoLangFedora4ClientIT.java by Remi Malessa
+     * @since 4.4.1-SNAPSHOT
+     */
+    @Test
+    public void testExists() {
+
+        final String id = UUID.randomUUID().toString();
+        assertFalse("Object should not exist: " + id, client.exists(id));
+
+        // TODO implement integration test with client.createResource(id) method
+
+        // TODO implement integration test with client.createVersionSnapshot(id, "v1")
+    }
+
+}
---
+++ src/test/java/org/fcrepo/migration/f4clients/OCFLFedora4ClientTest.java
@@ -0,0 +1,48 @@
+/**
+ *
+ */
+package org.fcrepo.migration.f4clients;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+/**
+ * @author Remigiusz Malessa
+ * @since 4.4.1-SNAPSHOT
+ */
+public class OCFLFedora4ClientTest {
+
+    private static OCFLFedora4Client client;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUp() throws Exception {
+
+        final String storage = "src/test/resources/ocflStorage";
+        final String staging = "src/test/resources/staging";
+        client = new OCFLFedora4Client(storage, staging, OCFLFedora4Client.ObjectIdMapperType.FLAT);
+    }
+
+
+    /**
+     * @author Remigiusz Malessa
+     * @since 4.4.1-SNAPSHOT
+     */
+    @Test
+    public void testExists() {
+
+        // Sanity check -
+        final String badId = "nonexistantobject";
+        assertFalse("Object should not exist: " + badId, client.exists(badId));
+
+        final String goodId = "o1";
+        assertTrue("Object should exist: " + goodId, client.exists(goodId));
+    }
+
+}
