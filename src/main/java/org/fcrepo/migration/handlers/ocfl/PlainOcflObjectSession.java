0e7c02aa4d5bc546040d74d6c68137c56be8a057
Peter Winckles <peter.winckles@wisc.edu>
2020-11-05 08:16:08 -0600

    stubbed new storage-ocfl method

--- src/main/java/org/fcrepo/migration/handlers/ocfl/PlainOcflObjectSession.java
+++ src/main/java/org/fcrepo/migration/handlers/ocfl/PlainOcflObjectSession.java
@@ -112,6 +112,11 @@ public class PlainOcflObjectSession implements OcflObjectSession {
     }
 
     @Override
+    public synchronized void writeHeaders(final ResourceHeaders headers) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
     public void versionCreationTimestamp(final OffsetDateTime timestamp) {
         versionInfo.setCreated(timestamp);
     }
