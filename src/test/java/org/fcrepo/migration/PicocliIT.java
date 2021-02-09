b1bd8d90d2e5f2777e2348649cc73ebf1e175e7c
Ben Cail <benjamin_cail@brown.edu>
2021-02-09 14:06:05 -0500

    fix failing test

--- src/test/java/org/fcrepo/migration/PicocliIT.java
+++ src/test/java/org/fcrepo/migration/PicocliIT.java
@@ -222,8 +222,8 @@ public class PicocliIT {
         final int result = cmd.execute(args);
         assertEquals(0, result);
         assertTrue(Files.list(targetDir).anyMatch(element -> element.endsWith("0=ocfl_1.0")));
-        final Path baseDir = targetDir.resolve("5b5").resolve("62d").resolve("d69")
-                .resolve("5b562dd698f17e3198e007e6f77f9e48f20a556c6bae84e6fc8d98544831daa6");
+        final Path baseDir = targetDir.resolve("750").resolve("677").resolve("e9b")
+                .resolve("750677e9b953845ba5069d27a3775fbced186987fd0f4a8c968ac457a7d415a8");
         final File inventory = baseDir.resolve("inventory.json").toFile();
         assertTrue(inventory.exists());
         validateManifests(inventory, "SHA-256", baseDir);
